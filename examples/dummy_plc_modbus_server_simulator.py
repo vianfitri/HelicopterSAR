4431b7ebd335661de1bb9bea16672d9bfe556edc
vianfitri <viancukiep@gmail.com>
2026-09-16 13:44:14 +0700

    commit

--- examples/dummy_plc_modbus_server_simulator.py
+++ examples/dummy_plc_modbus_server_simulator.py
@@ -11,16 +11,16 @@ from pymodbus.server import StartTcpServer
 SERVER_HOST = "0.0.0.0"
 SERVER_PORT = 502
 
-# Inisialisasi Data Block Modbus
+# Inisialisasi Data Block Modbus (200 register)
 coils = ModbusSequentialDataBlock(0, [False] * 200)
 discrete_inputs = ModbusSequentialDataBlock(0, [False] * 200)
 holding_registers = ModbusSequentialDataBlock(0, [0] * 200)
 
 # Masukkan ke Slave Context
 slave_store = ModbusSlaveContext(
-    di=discrete_inputs,    # FC 2 / Discrete Inputs (X)
-    co=coils,              # FC 1 / Coils (Y)
-    hr=holding_registers,  # FC 3 / Holding Registers (V)
+    di=discrete_inputs,    # FC 2 (Discrete Inputs / X)
+    co=coils,              # FC 1 (Coils / Y)
+    hr=holding_registers,  # FC 3 (Holding Registers / V)
     zero_mode=True
 )
 
@@ -37,35 +37,35 @@ def simulate_plc_data():
 
     while True:
         try:
-            # 1. Update Discrete Inputs X7 - X10 (FC 2)
+            # 1. Update Discrete Inputs X7 - X10 (Function Code = 2)
+            # Parameter posisional: (fx, address, values)
             x_values = [
                 random.choice([True, False]),  # X7 (FRWD1)
                 random.choice([True, False]),  # X8 (FRWD2)
                 random.choice([True, False]),  # X9 (RVRS1)
                 random.choice([True, False]),  # X10 (RVRS2)
             ]
-            # Di pymodbus 3.6.8 gunakan fc=2
-            slave_store.setValues(fc=2, address=7, values=x_values)
+            slave_store.setValues(2, 7, x_values)
 
-            # 2. Update Output Lampu Y8 - Y11 (FC 1)
+            # 2. Update Output Lampu Y8 - Y11 (Function Code = 1)
             y_values = [
                 random.choice([True, False]),  # Y8 (FRWD Lamp)
                 random.choice([True, False]),  # Y9 (RVRS Lamp)
                 random.choice([True, False]),  # Y10 (Hoist Up Lamp)
                 random.choice([True, False]),  # Y11 (Hoist Down Lamp)
             ]
-            slave_store.setValues(fc=1, address=8, values=y_values)
+            slave_store.setValues(1, 8, y_values)
 
-            # 3. Update Register V32, V34, V102 (FC 3)
+            # 3. Update Register V32, V34, V102 (Function Code = 3)
             rpm = max(800, min(1800, rpm + random.randint(-50, 50)))
             speed2 = max(10, min(100, speed2 + random.randint(-5, 5)))
             hoist_load = max(
                 100, min(1000, hoist_load + random.randint(-20, 20))
             )
 
-            slave_store.setValues(fc=3, address=32, values=[rpm])         # V32 (RPM)
-            slave_store.setValues(fc=3, address=34, values=[speed2])      # V34 (SPEED2)
-            slave_store.setValues(fc=3, address=102, values=[hoist_load]) # V102 (Hoist Load)
+            slave_store.setValues(3, 32, [rpm])         # V32 (RPM)
+            slave_store.setValues(3, 34, [speed2])      # V34 (SPEED2)
+            slave_store.setValues(3, 102, [hoist_load]) # V102 (Hoist Load)
 
             time.sleep(1)
 
